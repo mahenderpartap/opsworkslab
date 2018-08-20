cdc4c85fbe73ef83cb30a46334dddded446fa71c
mahenderpartap <42511660+mahenderpartap@users.noreply.github.com>
2018-08-20 07:54:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Welcome to OpsWorks LAB</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>AWS OPSWORKS LAB</h1>
+                <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
+                <div align="center"><img src="https://s3.ap-south-1.amazonaws.com/lkm-opsworkslab/OpsWorkslogo.jpg"></div>
+
+                <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
